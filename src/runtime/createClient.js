864f88d5c25899f6ed298b425066a97f1562d9e2
Izaak Schroeder <izaak.schroeder@gmail.com>
2018-07-13 13:30:35 -0700

    Few remaining tweaks.

--- src/runtime/createClient.js
+++ src/runtime/createClient.js
@@ -2,21 +2,17 @@
 import ApolloClient from 'apollo-client';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 import {WebSocketLink} from 'apollo-link-ws';
-import urlJoin from 'url-join';
 
 import WebSocket from './WebSocket';
 
-const createClient = (url?: string): ApolloClient => {
+const createClient = (url: string): ApolloClient => {
   if (typeof url === 'undefined') {
-    if (typeof global.__webpack_udev_client__ !== 'undefined') {
-      return global.__webpack_udev_client__;
-    }
     throw new TypeError('Must provide valid `url`.');
   }
 
   const cache = new InMemoryCache();
   const link = new WebSocketLink({
-    uri: urlJoin(url.replace(/^http/, 'ws'), '/__webpack_udev_graphql__'),
+    uri: url.replace(/^http/, 'ws'),
     options: {
       reconnect: true,
     },
